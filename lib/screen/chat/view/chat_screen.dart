ade8178a4384e078c1e06308b022ab345fcb2dbe
Admin <admin@Admins-MacBook-Pro.local>
2024-09-11 09:20:37 +0530

    new

--- lib/screen/chat/view/chat_screen.dart
+++ lib/screen/chat/view/chat_screen.dart
@@ -69,118 +69,123 @@ class _ChatScreenState extends State<ChatScreen> {
             width: MediaQuery.sizeOf(context).width,
             fit: BoxFit.cover,
           ),
-          StreamBuilder(
-              stream: controller.dataSnap,
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                } else if (snapshot.hasData) {
-                  List<ChatModel> chatList = [];
-                  QuerySnapshot? snap = snapshot.data;
-                  for (var x in snap!.docs) {
-                    Map m1 = x.data() as Map;
-                    ChatModel c1 = ChatModel.mapToModel(m1);
-                    c1.docId = x.id;
-                    chatList.add(c1);
-                  }
-                  return Expanded(
-                    child: ListView.builder(
-                      itemCount: chatList.length,
-                      itemBuilder: (context, index) {
-                        return Container(
-                          height: 50,
-                          margin: const EdgeInsets.all(5),
-                          width: 200,
-                          alignment: chatList[index].senderId !=
-                                  AuthHelper.helper.user!.uid
-                              ? Alignment.centerLeft
-                              : Alignment.centerRight,
-                          child: InkWell(
-                            onLongPress: () {
-                              if (chatList[index].senderId ==
-                                  AuthHelper.helper.user!.uid) {
-                                Get.defaultDialog(
-                                    title: "Chatify",
-                                    middleText:
-                                        "Are you sure want to delete this message?",
-                                    actions: [
-                                      TextButton(
-                                          onPressed: () {
-                                            Get.back();
-                                          },
-                                          child: const Text("Cancel")),
-                                      TextButton(
-                                          onPressed: () async {
-                                            await FireDbHelper.helper
-                                                .deleteChat(
-                                                    chatList[index].docId!);
-                                            Get.back();
-                                          },
-                                          child: const Text("Delete")),
-                                    ]);
-                              }
-                            },
-                            child: Container(
-                              width: MediaQuery.sizeOf(context).width * 0.50,
-                              alignment: Alignment.centerLeft,
-                              decoration: BoxDecoration(
-                                color: fav,
-                                borderRadius: chatList[index].senderId !=
-                                        AuthHelper.helper.user!.uid
-                                    ? const BorderRadius.only(
-                                        topLeft: Radius.circular(10),
-                                        bottomRight: Radius.circular(10))
-                                    : const BorderRadius.only(
-                                        topLeft: Radius.circular(10),
-                                        bottomRight: Radius.circular(10)),
+          Column(
+            children: [
+              Expanded(
+                child: StreamBuilder(
+                    stream: controller.dataSnap,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      } else if (snapshot.hasData) {
+                        List<ChatModel> chatList = [];
+                        QuerySnapshot? snap = snapshot.data;
+                        for (var x in snap!.docs) {
+                          Map m1 = x.data() as Map;
+                          ChatModel c1 = ChatModel.mapToModel(m1);
+                          c1.docId = x.id;
+                          chatList.add(c1);
+                        }
+                        return ListView.builder(
+                          itemCount: chatList.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              height: 50,
+                              margin: const EdgeInsets.all(5),
+                              width: 200,
+                              alignment: chatList[index].senderId !=
+                                      AuthHelper.helper.user!.uid
+                                  ? Alignment.centerLeft
+                                  : Alignment.centerRight,
+                              child: InkWell(
+                                onLongPress: () {
+                                  if (chatList[index].senderId ==
+                                      AuthHelper.helper.user!.uid) {
+                                    Get.defaultDialog(
+                                        title: "Chatify",
+                                        middleText:
+                                            "Are you sure want to delete this message?",
+                                        actions: [
+                                          TextButton(
+                                              onPressed: () {
+                                                Get.back();
+                                              },
+                                              child: const Text("Cancel")),
+                                          TextButton(
+                                              onPressed: () async {
+                                                await FireDbHelper.helper
+                                                    .deleteChat(
+                                                        chatList[index].docId!);
+                                                Get.back();
+                                              },
+                                              child: const Text("Delete")),
+                                        ]);
+                                  }
+                                },
+                                child: Container(
+                                  width: MediaQuery.sizeOf(context).width * 0.50,
+                                  alignment: Alignment.centerLeft,
+                                  decoration: BoxDecoration(
+                                    color: fav,
+                                    borderRadius: chatList[index].senderId !=
+                                            AuthHelper.helper.user!.uid
+                                        ? const BorderRadius.only(
+                                            topLeft: Radius.circular(10),
+                                            bottomRight: Radius.circular(10))
+                                        : const BorderRadius.only(
+                                            topLeft: Radius.circular(10),
+                                            bottomRight: Radius.circular(10)),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 12),
+                                    child: Text("${chatList[index].msg}"),
+                                  ),
+                                ),
                               ),
-                              child: Padding(
-                                padding: const EdgeInsets.only(left: 12),
-                                child: Text("${chatList[index].msg}"),
-                              ),
-                            ),
-                          ),
+                            );
+                          },
                         );
-                      },
-                    ),
-                  );
-                }
-                return const CircularProgressIndicator();
-              }),
-          //const SearchBar(),
-          Card(
-            color: fav,
-            margin: const EdgeInsets.all(15),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: txtChat,
-                    decoration: const InputDecoration(
-                      hintStyle: TextStyle(wordSpacing: 1),
-                      hintText: "Write message",
-                    ),
-                  ),
+                      }
+                      return const CircularProgressIndicator();
+                    }),
+              ),
+              Card(
+                color: fav,
+                margin: const EdgeInsets.all(15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
                 ),
-                const Icon(Icons.keyboard_voice),
-                IconButton(
-                  onPressed: () {
-                    ChatModel chatModel = ChatModel(
-                        dateTime: Timestamp.now(),
-                        msg: txtChat.text,
-                        senderId: AuthHelper.helper.user!.uid);
-                    FireDbHelper.helper.sendMessage(
-                        AuthHelper.helper.user!.uid, "${model.uid}", chatModel);
-                  },
-                  icon: const Icon(Icons.send),
-                  color: Colors.white,
-                )
-              ],
-            ),
-          )
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: txtChat,
+                        decoration: const InputDecoration(
+                          hintStyle: TextStyle(wordSpacing: 1),
+                          hintText: "Write message",
+                        ),
+                      ),
+                    ),
+                    const Icon(Icons.keyboard_voice),
+                    IconButton(
+                      onPressed: () {
+                        ChatModel chatModel = ChatModel(
+                            dateTime: Timestamp.now(),
+                            msg: txtChat.text,
+                            senderId: AuthHelper.helper.user!.uid);
+                        FireDbHelper.helper.sendMessage(
+                            AuthHelper.helper.user!.uid, "${model.uid}", chatModel);
+                      },
+                      icon: const Icon(Icons.send),
+                      color: Colors.white,
+                    )
+                  ],
+                ),
+              )
+              
+            ],
+          ),
+          //const SearchBar(),
         ],
       ),
     );
